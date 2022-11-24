7542515fe7e0275dbe5cfa81e9dabfe79cf077d7
Rigorevich Ilya <rigorevich268@gmail.com>
2022-11-24 18:22:20 +0300

    'fix'

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,7 +1,7 @@
 export const findTag = (str: string) => {
-  return str.match(/#[wа-яА-Я\d]+/g);
+  return str.match(/#[wа-яА-Яa-zA-Z\d]+/g);
 };
 // /#[^.,?! \(\)]+/g
 export const splitText = (str: string) => {
-  return str.split(/#[wа-яА-Я\d]+/);
+  return str.split(/#[wа-яА-Яa-zA-Z\d]+/);
 };
